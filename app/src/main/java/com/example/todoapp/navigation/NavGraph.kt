89ae7942c28e01d1eeda2234bfab892a800e9ab0
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-02-18 13:50:17 -0600

    Setup navigation part. 2

--- app/src/main/java/com/example/todoapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/todoapp/navigation/NavGraph.kt
@@ -2,8 +2,13 @@ package com.example.todoapp.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.todoapp.util.Constants.LIST_ARGUMENT_KEY
+import com.example.todoapp.util.Constants.TASK_ARGUMENT_KEY
 
 @Composable
 fun NavGraph(
@@ -11,5 +16,28 @@ fun NavGraph(
 ) {
     val screen = remember(navController) {
         Screens(navController = navController)
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.LIST.route,
+    ) {
+        composable(
+            route = Screen.LIST.route,
+            arguments = listOf(navArgument(LIST_ARGUMENT_KEY) {
+                type = NavType.StringType
+            })
+        ) {
+
+        }
+
+        composable(
+            route = Screen.TASK.route,
+            arguments = listOf(navArgument(TASK_ARGUMENT_KEY) {
+                type = NavType.IntType
+            })
+        ) {
+
+        }
     }
 }
