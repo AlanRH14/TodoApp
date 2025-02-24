741474b0487c60a22aa3d8d82f2c6392b44a2fd9
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-02-24 16:52:30 -0600

    Setup TASK SCREEN part. 2

--- app/src/main/java/com/example/todoapp/presentation/screens/task/components/NewTaskAppBar.kt
+++ app/src/main/java/com/example/todoapp/presentation/screens/task/components/NewTaskAppBar.kt
@@ -1,17 +1,67 @@
 package com.example.todoapp.presentation.screens.task.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todoapp.R
+import com.example.todoapp.ui.theme.TopAppBarBackgroundColor
+import com.example.todoapp.ui.theme.TopAppBarContentColor
+import com.example.todoapp.util.Action
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewTaskAppBar() {
+fun NewTaskAppBar(
+    navigationToListScreen: (Action) -> Unit,
+) {
     TopAppBar(
         title = {
+            Text(
+                stringResource(R.string.add_task_title),
+                color = TopAppBarContentColor,
+            )
         },
         navigationIcon = {
+            IconButton(
+                onClick = { navigationToListScreen(Action.NO_ACTION) }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back_arrow_button),
+                    tint = TopAppBarContentColor
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = { navigationToListScreen(Action.ADD) }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = stringResource(R.string.add_task_title),
+                    tint = TopAppBarContentColor
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = TopAppBarBackgroundColor
+        ),
+    )
+}
 
-        }
+
+@Composable
+@Preview
+fun NewTaskAppBarPreview() {
+    NewTaskAppBar(
+        navigationToListScreen = {}
     )
 }
