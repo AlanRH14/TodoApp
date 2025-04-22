1e96797a291732adc05cd62093b094e8f4984492
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-04-22 09:45:34 -0600

    Swipe to Delete - Implementation

--- app/src/main/java/com/example/todoapp/presentation/screens/list/widgets/ListContent.kt
+++ app/src/main/java/com/example/todoapp/presentation/screens/list/widgets/ListContent.kt
@@ -1,14 +1,18 @@
 package com.example.todoapp.presentation.screens.list.widgets
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.example.todoapp.data.model.Priority
 import com.example.todoapp.data.model.ToDoTask
 import com.example.todoapp.presentation.screens.list.components.TaskItem
+import com.example.todoapp.util.Action
 import com.example.todoapp.util.SearchAppBarState
 
 @Composable
@@ -20,6 +24,7 @@ fun ListContent(
     sortState: Priority,
     searchedTasks: List<ToDoTask>,
     searchAppBarState: SearchAppBarState,
+    onSwipeToDelete: (Action, ToDoTask) -> Unit,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     when {
@@ -27,6 +32,7 @@ fun ListContent(
             HandleListContent(
                 modifier = modifier,
                 tasks = searchedTasks,
+                onSwipeToDelete = onSwipeToDelete,
                 navigateToTaskScreen = navigateToTaskScreen
             )
         }
@@ -35,6 +41,7 @@ fun ListContent(
             HandleListContent(
                 modifier = modifier,
                 tasks = allTasks,
+                onSwipeToDelete = onSwipeToDelete,
                 navigateToTaskScreen = navigateToTaskScreen
             )
         }
@@ -43,6 +50,7 @@ fun ListContent(
             HandleListContent(
                 modifier = modifier,
                 tasks = lowPriorityTasks,
+                onSwipeToDelete = onSwipeToDelete,
                 navigateToTaskScreen = navigateToTaskScreen
             )
         }
@@ -51,6 +59,7 @@ fun ListContent(
             HandleListContent(
                 modifier = modifier,
                 tasks = highPriorityTasks,
+                onSwipeToDelete = onSwipeToDelete,
                 navigateToTaskScreen = navigateToTaskScreen
             )
         }
@@ -61,6 +70,7 @@ fun ListContent(
 private fun HandleListContent(
     modifier: Modifier = Modifier,
     tasks: List<ToDoTask>,
+    onSwipeToDelete: (Action, ToDoTask) -> Unit,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     if (tasks.isEmpty()) {
@@ -77,17 +87,36 @@ private fun HandleListContent(
                     task.id
                 }
             ) { task ->
-                /*TaskItem(
-                    toDoTask = task,
-                    navigationToTaskScreen = navigateToTaskScreen
-                )*/
+                val dismissState = rememberSwipeToDismissBoxState(
+                    confirmValueChange = {
+                        when (it) {
+                            SwipeToDismissBoxValue.EndToStart -> {
+                                onSwipeToDelete(Action.DELETE, task)
+                                return@rememberSwipeToDismissBoxState true
+                            }
 
-                val dismissState = rememberSwipeToDismissBoxState()
+                            else -> return@rememberSwipeToDismissBoxState false
+                        }
+                    },
+                    positionalThreshold = { it * .30f }
+                )
 
+                val degrees by animateFloatAsState(
+                    if (dismissState.targetValue == SwipeToDismissBoxValue.Settled) {
+                        0F
+                    } else {
+                        -45F
+                    }
+                )
                 SwipeToDismissBox(
                     state = dismissState,
-                    enableDismissFromEndToStart = true,
-                ) { }
+                    backgroundContent = { RedBackground(degrees) }
+                ) {
+                    TaskItem(
+                        toDoTask = task,
+                        navigationToTaskScreen = navigateToTaskScreen
+                    )
+                }
             }
         }
     }
