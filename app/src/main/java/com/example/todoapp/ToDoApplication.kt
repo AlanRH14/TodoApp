c9eeb9e1d9c205a37a4a179cf2cb308133cd72fb
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-08 13:54:48 -0600

    Change hilt to koin part. 9.

--- app/src/main/java/com/example/todoapp/ToDoApplication.kt
+++ app/src/main/java/com/example/todoapp/ToDoApplication.kt
@@ -3,6 +3,7 @@ package com.example.todoapp
 import android.app.Application
 import com.example.todoapp.di.appModule
 import com.example.todoapp.di.databaseModule
+import com.example.todoapp.di.repositoryModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -15,6 +16,7 @@ class ToDoApplication : Application() {
             androidContext(this@ToDoApplication)
             modules(
                 databaseModule,
+                repositoryModule,
                 appModule
             )
         }
