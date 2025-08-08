74d2c5cf6b79c427500994fd50aff5b7cdf1e3ab
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-08 13:40:11 -0600

    Change hilt to koin part. 3.

--- app/src/main/java/com/example/todoapp/ToDoApplication.kt
+++ app/src/main/java/com/example/todoapp/ToDoApplication.kt
@@ -2,9 +2,9 @@ package com.example.todoapp
 
 import android.app.Application
 import com.example.todoapp.di.databaseModule
-import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+
 class ToDoApplication : Application() {
 
     override fun onCreate() {
