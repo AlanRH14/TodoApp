da5907ee809b9529eede4a8863b2644ddfc1a3e8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-11 07:56:53 -0600

    Change Data Store with koin part. 4.

--- app/src/main/java/com/example/todoapp/data/repositories/DataRepository.kt
+++ app/src/main/java/com/example/todoapp/data/repositories/DataRepository.kt
@@ -1,33 +1,24 @@
 package com.example.todoapp.data.repositories
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.IOException
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.example.todoapp.data.model.Priority
 import com.example.todoapp.util.Constants.PREFERENCE_KEY
-import com.example.todoapp.util.Constants.PREFERENCE_NAME
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-import javax.inject.Inject
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = PREFERENCE_NAME)
-
-class DataRepository @Inject constructor(
-    @ApplicationContext private val mContext: Context
+class DataRepository(
+    private val dataStore: DataStore<Preferences>
 ) {
 
     private object PreferenceKey {
         val sortKey = stringPreferencesKey(name = PREFERENCE_KEY)
     }
-
-    private val dataStore = mContext.dataStore
 
     suspend fun persistStore(priority: Priority) {
         dataStore.edit { preference ->
@@ -36,13 +27,13 @@ class DataRepository @Inject constructor(
     }
 
     val readSortState: Flow<String> = dataStore.data
-        .catch { exception  ->
+        .catch { exception ->
             if (exception is IOException) {
                 emit(emptyPreferences())
             } else {
                 throw exception
             }
-        }.map {preferences ->
+        }.map { preferences ->
             preferences[PreferenceKey.sortKey] ?: Priority.NONE.name
         }
 }
