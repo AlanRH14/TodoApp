91c4b7c549a41235829c2035cfca3b4d8b6dcfa4
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-11 12:38:31 -0600

    Add MVI config in project part. 23.

--- app/src/main/java/com/example/todoapp/presentation/screens/list/ListViewModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/screens/list/ListViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.todoapp.presentation.screens.list
 
 import androidx.lifecycle.ViewModel
+import com.example.todoapp.domain.repository.DataStoreRepository
+import com.example.todoapp.domain.repository.ToDoRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class ListViewModel : ViewModel() {
+class ListViewModel(
+    private val repository: ToDoRepository,
+    private val dataStoreRepository: DataStoreRepository
+) : ViewModel() {
 
     private val _state = MutableStateFlow(ListState())
     val state = _state.asStateFlow()
