a560be44005ef25d48d6bf7f2a600447613f1b0b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-11 12:24:25 -0600

    Add MVI config in project part. 19.

---
+++ app/src/main/java/com/example/todoapp/presentation/screens/list/ListState.kt
@@ -0,0 +1,6 @@
+package com.example.todoapp.presentation.screens.list
+
+data class ListState(
+    val isLoading: Boolean = false,
+    val isError: String = ""
+)
---
+++ app/src/main/java/com/example/todoapp/presentation/screens/list/ListViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.todoapp.presentation.screens.list
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ListViewModel : ViewModel() {
+
+    private val _state = MutableStateFlow(ListState())
+    val state = _state.asStateFlow()
+
+
+    fun onEvent() {}
+
+}
