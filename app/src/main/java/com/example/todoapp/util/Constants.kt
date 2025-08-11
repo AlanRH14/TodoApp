ba48fb0cee9cbb680868b51c580502d04e0e93eb
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-11 10:43:59 -0600

    Add MVI config in project part. 16.

--- app/src/main/java/com/example/todoapp/util/Constants.kt
+++ app/src/main/java/com/example/todoapp/util/Constants.kt
@@ -1,12 +1,8 @@
 package com.example.todoapp.util
 
 object Constants {
-
     const val DATABASE_NAME = "todo_database"
     const val DATABASE_TABLE = "todo_table"
 
     const val MAX_TITLE_LENGTH = 20
-
-    const val PREFERENCE_NAME = "todo_preference"
-    const val PREFERENCE_KEY = "sort_state"
 }
--- app/src/main/java/com/example/todoapp/util/PreferencesConstants.kt
+++ app/src/main/java/com/example/todoapp/util/PreferencesConstants.kt
@@ -1,5 +1,8 @@
 package com.example.todoapp.util
 
 object PreferencesConstants {
+    const val PREFERENCE_NAME = "todo_preference"
+
+    const val PREFERENCE_KEY = "sort_state"
     const val PRIORITY = "priority"
 }
