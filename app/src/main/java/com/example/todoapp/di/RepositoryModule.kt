d7514b142943d46705dcb3b9a90042298b57ea31
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-11 09:05:55 -0600

    Change Data Store with koin part. 15.

--- app/src/main/java/com/example/todoapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/todoapp/di/RepositoryModule.kt
@@ -1,10 +1,14 @@
 package com.example.todoapp.di
 
+import com.example.todoapp.data.repositories.DataRepositoryImpl
 import com.example.todoapp.data.repositories.ToDoRepository
+import com.example.todoapp.domain.repository.DataStoreRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<ToDoRepository> {
         ToDoRepository(get())
     }
+
+    single<DataStoreRepository> { DataRepositoryImpl(dataStore = get()) }
 }
