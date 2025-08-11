33be76cfa5fb023734b62484a624fc3437f02637
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-11 12:27:35 -0600

    Add MVI config in project part. 22.

--- app/src/main/java/com/example/todoapp/presentation/screens/list/ListViewModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/screens/list/ListViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.todoapp.presentation.screens.list
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class ListViewModel : ViewModel() {
@@ -9,5 +11,10 @@ class ListViewModel : ViewModel() {
     private val _state = MutableStateFlow(ListState())
     val state = _state.asStateFlow()
 
-    fun onEvent() {}
+    private val _effect = MutableSharedFlow<ListEffect>()
+    val effect = _effect.asSharedFlow()
+
+    fun onEvent(event: ListUIEvent) {
+
+    }
 }
