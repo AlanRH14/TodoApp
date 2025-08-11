e68f5cbcf0d1ac59e1775a1611c33f0b77093066
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-11 10:15:27 -0600

    Add MVI config in project part. 4.

---
+++ app/src/main/java/com/example/todoapp/domain/repository/ToDoRepository.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp.domain.repository
+
+import com.example.todoapp.data.model.ToDoTask
+import com.example.todoapp.util.RequestState
+import kotlinx.coroutines.flow.Flow
+
+interface ToDoRepository {
+
+    fun sortByLowPriority(): Flow<RequestState<List<ToDoTask>>>
+
+    fun sortByHighPriority(): Flow<RequestState<List<ToDoTask>>>
+
+    fun getAllTasks(): Flow<RequestState<List<ToDoTask>>>
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+
+    suspend fun addTask(toDoTask: ToDoTask)
+
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    suspend fun deleteAllTasks()
+
+    fun searchTask(searchQuery: String): Flow<RequestState<List<ToDoTask>>>
+}
