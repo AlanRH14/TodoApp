d9c655b4dee6c5594431db313a6d88d81f5da580
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-13 17:01:05 -0600

    Add MVI config in project part. 72.

--- app/src/main/java/com/example/todoapp/di/AppModule.kt
+++ app/src/main/java/com/example/todoapp/di/AppModule.kt
@@ -2,7 +2,6 @@ package com.example.todoapp.di
 
 
 import com.example.todoapp.presentation.viewmodel.ListViewModel
-import com.example.todoapp.presentation.viewmodel.SharedViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
