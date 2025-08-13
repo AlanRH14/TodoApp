911fcc2b1ddeb1a5f8040b4dab5492e8979b5f54
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-13 14:52:22 -0600

    Add MVI config in project part. 69.

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.todoapp.navigation.NavGraph
-import com.example.todoapp.presentation.viewmodel.SharedViewModel
 import com.example.todoapp.ui.theme.TodoAppTheme
-import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
 
