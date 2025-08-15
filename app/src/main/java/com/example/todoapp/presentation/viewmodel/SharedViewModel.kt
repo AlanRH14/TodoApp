61c9bc0727db27a5ea972c6150499df5f07a7007
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-15 10:37:59 -0600

    Fix errors part. 11.

--- app/src/main/java/com/example/todoapp/presentation/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/viewmodel/SharedViewModel.kt
@@ -8,7 +8,7 @@ import com.example.todoapp.data.model.Priority
 import com.example.todoapp.domain.repository.DataStoreRepository
 import com.example.todoapp.domain.repository.ToDoRepository
 import com.example.todoapp.presentation.mvi.ListEffect
-import com.example.todoapp.presentation.screens.list.ListState
+import com.example.todoapp.presentation.mvi.ListState
 import com.example.todoapp.presentation.screens.list.ListUIEvent
 import com.example.todoapp.util.Action
 import com.example.todoapp.util.Constants
