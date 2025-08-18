c63d41d532af497b11628d268d499ecb05017a0f
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-18 13:53:33 -0600

    Fix errors part. 42.

--- app/src/main/java/com/example/todoapp/di/MapperModule.kt
+++ app/src/main/java/com/example/todoapp/di/MapperModule.kt
@@ -5,14 +5,15 @@ import com.example.todoapp.data.ToDoTaskDomainMapperImpl
 import com.example.todoapp.data.ToDoTaskEntityMapperImpl
 import com.example.todoapp.data.local.database.entities.ToDoTaskEntity
 import com.example.todoapp.domain.ToDoTask
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val mapperModule = module {
-    single<GenericMapper<ToDoTaskEntity, ToDoTask>> {
+    single<GenericMapper<ToDoTaskEntity, ToDoTask>>(named("ToDoTaskEntityMapperImpl")) {
         ToDoTaskEntityMapperImpl()
     }
 
-    single<GenericMapper<ToDoTask, ToDoTaskEntity>> {
+    single<GenericMapper<ToDoTask, ToDoTaskEntity>>(named("ToDoTaskDomainMapperImpl")) {
         ToDoTaskDomainMapperImpl()
     }
 }
--- app/src/main/java/com/example/todoapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/todoapp/di/RepositoryModule.kt
@@ -4,14 +4,15 @@ import com.example.todoapp.data.local.preferences.DataStoreRepositoryImpl
 import com.example.todoapp.data.repositories.ToDoRepositoryImpl
 import com.example.todoapp.domain.repository.DataStoreRepository
 import com.example.todoapp.domain.repository.ToDoRepository
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<ToDoRepository> {
         ToDoRepositoryImpl(
             toDoDao = get(),
-            entityMapper = get(),
-            domainMapper = get()
+            entityMapper = get(named("ToDoTaskEntityMapperImpl")),
+            domainMapper = get(named("ToDoTaskDomainMapperImpl"))
         )
     }
 
