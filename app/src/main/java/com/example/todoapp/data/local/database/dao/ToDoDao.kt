f86a9be3ab2a0e8ade9dfafed85a9e26547ac8b9
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-18 09:17:28 -0600

    Fix errors part. 24.

--- app/src/main/java/com/example/todoapp/data/local/database/dao/ToDoDao.kt
+++ app/src/main/java/com/example/todoapp/data/local/database/dao/ToDoDao.kt
@@ -13,20 +13,28 @@ import kotlinx.coroutines.flow.Flow
 interface ToDoDao {
     @Query("SELECT * FROM todo_table ORDER BY id ASC")
     fun getAllTasks(): Flow<List<ToDoTaskEntity>>
+
     @Query("SELECT * FROM todo_table WHERE id=:taskId")
     fun getSelectedTask(taskId: Int): Flow<ToDoTaskEntity>
+
     @Insert(onConflict = OnConflictStrategy.Companion.REPLACE)
     suspend fun addTask(toDoTaskEntity: ToDoTaskEntity)
+
     @Update
     suspend fun updateTask(toDoTaskEntity: ToDoTaskEntity)
+
     @Delete
     suspend fun deleteTask(toDoTaskEntity: ToDoTaskEntity)
+
     @Query("DELETE FROM todo_table")
     suspend fun deleteAllTasks()
+
     @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
     fun searchDatabase(searchQuery: String): Flow<List<ToDoTaskEntity>>
+
     @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
     fun sortByLowPriority(): Flow<List<ToDoTaskEntity>>
+
     @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
     fun sortByHighPriority(): Flow<List<ToDoTaskEntity>>
 }
