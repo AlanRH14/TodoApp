3a797cc66f2a1246b0ac19836f5b50ffcc662005
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-20 11:37:20 -0600

    Fix errors part. 69.

--- app/src/main/java/com/example/todoapp/presentation/screens/task/widgets/TaskAppBar.kt
+++ app/src/main/java/com/example/todoapp/presentation/screens/task/widgets/TaskAppBar.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.res.stringResource
 import com.example.todoapp.R
 import com.example.todoapp.domain.ToDoTask
 import com.example.todoapp.presentation.components.DisplayAlertDialog
-import com.example.todoapp.presentation.screens.list.mvi.ListUIEvent
 import com.example.todoapp.presentation.screens.task.TaskUIEvent
 import com.example.todoapp.presentation.screens.task.components.ExistingTaskAppBar
 import com.example.todoapp.presentation.screens.task.components.NewTaskAppBar
