65d337682468d99ce77c5d815eae654fdd5a8f24
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-08-20 09:50:41 -0600

    Fix errors part. 64.

--- app/src/main/java/com/example/todoapp/presentation/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/viewmodel/TaskViewModel.kt
@@ -6,7 +6,7 @@ import com.example.todoapp.data.model.Priority
 import com.example.todoapp.domain.ToDoTask
 import com.example.todoapp.domain.repository.ToDoRepository
 import com.example.todoapp.presentation.screens.list.mvi.ListEffect
-import com.example.todoapp.presentation.mvi.ListState
+import com.example.todoapp.presentation.screens.list.mvi.ListState
 import com.example.todoapp.presentation.mvi.ListUIEvent
 import com.example.todoapp.util.Action
 import com.example.todoapp.util.Constants
